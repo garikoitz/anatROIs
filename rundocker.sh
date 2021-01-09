332bd4d60b48950fc78ea9bbd18276998b0ab3bd
garikoitz <garikoitz@gmail.com>
2021-01-08 16:08:27 -0800

    add optional FS and optional MNI rois zip file

--- rundocker.sh
+++ rundocker.sh
@@ -2,4 +2,4 @@ docker run -ti --rm  $4 \
 	       -v $2/input:/flywheel/v0/input  \
    	       -v $2/output:/flywheel/v0/output  \
            -v $3:/flywheel/v0/config.json \
-   	       garikoitz/freesurfer-recon-all:$1
+   	       garikoitz/anatrois:$1
